4eadba37c37864cbc83333a28e8661148ebd7600
voidpls <RazeLegendz@Razor.fios-router.home>
2017-06-17 13:42:22 -0400

    up

--- discordiebot.js
+++ discordiebot.js
@@ -151,7 +151,7 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
   globalon_message('..nigger', 'http://www.voidpls.tk/files/nigger.jpg');
   globalon_message('..gas', ':star_of_david: **Gas the Kikes** :star_of_david:');
   globalon_message('..heil', 'http://www.voidpls.tk/files/hitler.png');
-  on_message('..remind2', '**--------------------------------\n<:swastika:322900266959765506>   Daily Reminder:   <:swastika:322900266959765506>\nHitler Did Nothing Wrong!\n--------------------------------**');
+  on_message('..remind2', '**--------------------------------\n<:swastika:325668829759930368>   Daily Reminder:   <:swastika:325668829759930368>\nHitler Did Nothing Wrong!\n--------------------------------**');
   on_message('..remind', '**--------------------------------\n<:swastikaflaghitler:325352487148060672>   Daily Reminder:   <:swastikaflaghitler:325352487148060672>\nHitler Did Nothing Wrong!\n--------------------------------**');
 
 //autodelete
