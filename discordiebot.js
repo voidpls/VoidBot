9235404a00c14a6988e4dbf48fa61e18b3ba5e29
voidpls <RazeLegendz@Razor.fios-router.home>
2017-06-19 15:07:19 -0400

    update

--- discordiebot.js
+++ discordiebot.js
@@ -120,6 +120,7 @@ client.Dispatcher.on("GATEWAY_READY", e => {
 });
 
 client.Dispatcher.on("GUILD_MEMBER_ADD", e => {
+  var guild = client.Guilds.find(g => g.id == '325315599708454913');
   var channel = e.guild.channels('325648252810690570')
 	if (e.guild.id === "190646276256169994") {
 		channel.sendMessage(`Welcome to Moon Central, ${e.member.username}!`);
