fb3089c00dfc056b80d2c61e33fb6b8afe056e75
voidpls <RazeLegendz@Razor.fios-router.home>
2017-06-23 16:14:47 -0400

    up

--- discordiebot.js
+++ discordiebot.js
@@ -120,10 +120,10 @@ client.Dispatcher.on("GUILD_MEMBER_ADD", e => {
 };
 });
 
-client.Dispatcher.on("GUILD_MEMBER_REMOVE", e => {
+client.Dispatcher.on("GUILD_MEMBER_REMOVE", member => {
   let channel = client.Channels.get('325321760939704320');
 	if (e.guild.id === "325315599708454913") {
-    channel.sendMessage(`About time that nigger ${e.member.mention} left or got kicked!`);
+    channel.sendMessage(`About time that nigger ${member.user} left or got kicked!`);
 };
 });
 
