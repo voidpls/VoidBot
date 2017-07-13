32c0c6ae760e2c5b689575fb0d31dfb8117be04b
voidpls <raze2736@gmail.com>
2017-07-13 19:38:07 -0400

    Add files via upload
    update, kick + stag

--- discordiebot.js
+++ discordiebot.js
@@ -7,6 +7,7 @@ var client = new Discordie({
   messageCacheLimit: 1000,
   autoReconnect: true
 });
+var p = '..'
 var data = require('./data.json')
 var redpill = data.redpill
 var holocaust = data.holocaust
@@ -146,27 +147,8 @@ client.Dispatcher.on("GUILD_MEMBER_ADD", e => {
     setTimeout(send, 5000);
   }
 });
-//VC notify
-client.Dispatcher.on("VOICE_CHANNEL_JOIN", e => {
 
-  let channel = client.Channels.get('333769317697060865');
 
-  if (e.channelId == '329434888157790210'){
-  channel.sendMessage('',false, {
-      color: 0xD00000,
-      author: {
-       name: e.user.username + e.user.discriminator,
-       icon_url: e.user.avatarURL
-      },
-      fields: [
-        {
-          name: "",
-          value: '**' + e.user.username + ' has joined VC**'
-        }
-      ]
-    });
-  }
-});
 
 //commands
 client.Dispatcher.on("MESSAGE_CREATE", e => {
@@ -175,41 +157,94 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
   var args = e.message.content.split(/[ ]+/);
   client.Users.fetchMembers()
 
+//tagspam mk. ii
+if (e.message.content.toLowerCase().startsWith(p + 'stag') && spamIDs.includes(e.message.author.id)) {
+  if (args.length > 2){
+    var number = parseInt(args.pop());
+    args.shift();
+    spamtext = args.join(' ');
+    e.message.delete();
+    while (number != 0){
+      e.message.channel.sendMessage(spamtext).then(m => {
+        m.delete();
+      })
+      number = number - 1
+    };
+  }
+  else return;
+};
+
+/*
+  //prune
+  if (e.message.content.startsWith("." + "d" && e.message.author.id == '325827542164439040')) {
+    let msgcount = parseInt(args[0]);
+
+    //e.message.channel.fetchMessages({limit: msgcount}).then(messages => e.message.channel.deleteMessage(messages));
+
+    e.message.channel.fetchMessages({limit: 100}).then(() => {
+    var msgArray = e.message.channel.messages.filter(m => m.channel.id == e.message.channel.id);
+    msgArray.length = msgcount + 1;
+    client.Messages.deleteMessages(msgArray).catch(console.log);
+  });
+
+      //let msgs = e.message.channel.fetchMessages({limit: 100}).then(msgs => {
+
+        //var msgArray = msgs.filter(m => m.author.id === '325827542164439040' || m.author.id === '334475825258823701')
+        //client.Messages.deleteMessages(msgArray).catch(console.log);
+      //})
+    }
+    */
 //self message function
   function on_message(arg1, arg2){
-    if (e.message.content == arg1 && ids.includes(e.message.author.id))
+    if (e.message.content.toLowerCase() == p + arg1 && ids.includes(e.message.author.id))
     e.message.channel.sendMessage(arg2);
   };
 
 //global message function
   function globalon_message(arg1, arg2){
-  if (e.message.content == arg1 && e.message.author.id != client.User.id)
+  if (e.message.content.toLowerCase() == p + arg1 &&
+      e.message.author.id != client.User.id)
+  e.message.channel.sendMessage(arg2);
+  }
+//global message + delete
+  function globaldel_message(arg1, arg2){
+    if (e.message.content.toLowerCase() == p + arg1 &&
+        e.message.author.id != client.User.id &&
+        e.message.author.bot != true){
+    e.message.delete();
     e.message.channel.sendMessage(arg2);
   };
+  }
+//ping
+  if (e.message.content.toLowerCase() == p + 'ping' && ids.includes(e.message.author.id)) {
+    e.message.channel.sendMessage("**Pong! :ping_pong:**")
+    };
 
 //log
   var mainacc = client.Users.get('325827542164439040');
   if (e.message.author.id == '218177032327135232'){
   var channel = client.Channels.get('327320793681756161');
-  channel.sendMessage(`Baecon said: \"${e.message.content}\"`);
+  channel.sendMessage('Baecon said: \"${e.message.content}\"');
 }
-  else if (e.message.content.includes('void') &&
+  else if (e.message.content.toLowerCase().includes('void') &&
   e.message.author.id != client.User.id &&
 e.message.author.id != '325827542164439040') {
   var channel = client.Channels.get('327331811292217347');
-  channel.sendMessage("`${e.message.author.mention} said: \"${e.message.content}\"`");
+  channel.sendMessage("`" + e.message.author.username + "` said: `\"" + e.message.content + "`\"");
 }
   else if (mainacc.isMentioned(e.message) && e.message.author.id != client.User.id){
   var channel = client.Channels.get('327331811292217347');
-  channel.sendMessage(`${e.message.author.mention} said: \"${e.message.content}\"`);
+  let me = client.Users.get('325827542164439040');
+  let content = e.message.content.replace('<@325827542164439040>', '@' + me.username)
+  channel.sendMessage("`" + e.message.author.username + "` said: `\"" + content + "`\"");
 };
 //poll
-  if (e.message.content.startsWith('..poll')) {
+  if (e.message.content.toLowerCase().startsWith(p + 'poll')) {
   let sentMsg = e.message.channel.sendMessage('**Poll:**' + args.join(" ").substring(6))
   return sentMsg
   e.message.delete();
 };
-  if (e.message.content.startsWith('**Poll:**') &&
+  if (e.message.content.toLowerCase().startsWith('**Poll:**') &&
   e.message.author.id == client.User.id){
   e.message.addReaction('✅').then(e.message.addReaction('❎'));
 };
@@ -220,12 +255,12 @@ e.message.author.id != '325827542164439040') {
 //random on message
 function random_on_message(arg, list){
   var i = Math.floor(list.length * Math.random());
-  if (e.message.content == arg)
+  if (e.message.content.toLowerCase() == arg)
   e.message.channel.sendMessage(list[i]);
 }
 
 //spam
-  if (e.message.content.startsWith('..spam') && spamIDs.includes(e.message.author.id)) {
+  if (e.message.content.toLowerCase().startsWith(p + 'spam') && spamIDs.includes(e.message.author.id)) {
     if (args.length == 3){
       var spamtext = args[1].replace(/_/g, ' ');
       var number = parseInt(args[2]);
@@ -242,12 +277,12 @@ function random_on_message(arg, list){
 };
 
 //level up
-  if (e.message.content.includes('leveled up!')){
+  if (e.message.content.toLowerCase().includes('leveled up!')){
   e.message.channel.sendMessage('***L-L-LEVEL UP!!!***');
   };
 
 //holocaust
-  if (e.message.content.startsWith('..holocaust')){
+  if (e.message.content.toLowerCase().startsWith(p + 'holocaust')){
     var intarg = parseInt(args[1]) - 1
 
     if (args.length == 1){
@@ -262,7 +297,7 @@ function random_on_message(arg, list){
 };
 
 //avatar
-  if (e.message.content.startsWith('..avatar')) {
+  if (e.message.content.toLowerCase().startsWith(p + 'avatar')) {
   var user = client.Users.find(u => u.id == e.message.author.id);
   var avatarurl = user.avatarURL.replace('.jpg', '.webp?size=1024');
   e.message.channel.sendMessage(avatarurl);
@@ -272,12 +307,12 @@ function random_on_message(arg, list){
   e.message.addReaction('🦊');
 }
 //echo
-  if (e.message.content.startsWith('..echo') &&
+  if (e.message.content.toLowerCase().startsWith(p + 'echo') &&
       spamIDs.includes(e.message.author.id)){
   e.message.channel.sendMessage(args.join(" ").substring(6));
   e.message.delete();
 }
-  else if (e.message.content.startsWith('..echo') &&
+  else if (e.message.content.toLowerCase().startsWith(p + 'echo') &&
       e.message.author.id != '299052998355714049')
       e.message.reply('nice try')
 //ur mom gay
@@ -285,12 +320,30 @@ function random_on_message(arg, list){
   if (content.startsWith('ur mom') || content.startsWith('ur mum') || content.startsWith('your mom') || content.startsWith('your mum') || content.startsWith('your mother'))
   e.message.channel.sendMessage('no u');
 
-//delete evidence xd
-  if (e.message.content == '.on <@299052998355714049>')
-  e.message.delete();
+//die
+  if (e.message.content.toLowerCase().startsWith('..kill') && e.message.author.id == '325827542164439040')
+  client.disconnect();
+//kick
+  if (e.message.content.toLowerCase().startsWith('..gas') && e.message.author.id == '325827542164439040'){
+    let user = getUser(args[1]);
+    if (user === undefined) return;
+    else {
+    user.kick();
+    e.message.channel.sendMessage("<:check:335197798272073728>** " + user.username + " **has been gassed!");
+  };
+  }
+//get user function
+  function getUser(arg) {
+    user = arg.replace(/\D/g,'');
+    user = e.message.guild.members.find(gUser => gUser.id === user);
 
+    if (user === undefined) {
+        return;
+    }
+    return user;
+}
 //help
-  if (e.message.content.toLowerCase() == '..help' && helpIDs.includes(e.message.guild.id)){
+  if (e.message.content.toLowerCase() == p + 'help' && helpIDs.includes(e.message.guild.id)){
   client.Users.getMember('261841687784062977', '323992245781135360')
   var user = client.User;
   var username = user.username;
@@ -317,25 +370,23 @@ function random_on_message(arg, list){
   if (e.message.content.toLowerCase().startsWith('traps are gay'))
   e.message.reply('but they aren\'t');
 //basic commands
-  on_message('..ping', '**Pong!** :ping_pong:');
-  on_message('..rep', 't!rep <@135721889828962305>');
-  on_message('..daily', 't!daily <@135721889828962305>');
-  on_message('..on', '.on <@299052998355714049>');
-  on_message('..swastika', swastika)
-  globalon_message('..morticia', 'http://www.voidpls.tk/files/morticia.jpg');
+  on_message('swastika', swastika)
+  globaldel_message('jihad', '<:jihad:322904816441491456>');
+  globaldel_message('911', '<:plane:334937403217281024><:towers:334934504647032832>');
+  globaldel_message('morticia', 'http://www.voidpls.tk/files/morticia.jpg');
   globalon_message('/o/', '\\o\\');
   globalon_message('\\o\\', '/o/');
-  globalon_message('..bob', '`..bob`? I think you mean `..gay faggot`');
-  globalon_message('..gay faggot', '`..gay faggot`? I think you mean `..bob`');
-  globalon_message('..gas', ':star_of_david: **Gas the Kikes** :star_of_david:');
-  globalon_message('..heil', 'http://www.voidpls.tk/files/hitler.png');
-  globalon_message('..truth', 'Girls are the best but like Effy is better');
-  on_message('..remind', '**--------------------------------\n<:swastika:325668829759930368>   Daily Reminder:   <:swastika:325668829759930368>\nHitler Did Nothing Wrong!\n--------------------------------**');
-  random_on_message('..diversity', links);
-  random_on_message('..nigger', niggers);
-  random_on_message('..redpill', redpill);
+  globalon_message('bob', '`bob`? I think you mean `gay faggot`');
+  globalon_message('gay faggot', '`gay faggot`? I think you mean `bob`');
+  globaldel_message('gas', ':star_of_david: **Gas the Kikes** :star_of_david:');
+  globalon_message('heil', 'http://www.voidpls.tk/files/hitler.png');
+  globalon_message('truth', 'Girls are the best but like Effy is better');
+  on_message('remind', '**--------------------------------\n<:swastika:325668829759930368>   Daily Reminder:   <:swastika:325668829759930368>\nHitler Did Nothing Wrong!\n--------------------------------**');
+  random_on_message('diversity', links);
+  random_on_message('nigger', niggers);
+  random_on_message('redpill', redpill);
 //islam
   let islamI = Math.floor(islam.length * Math.random());
-  if (e.message.content == '..islam')
+  if (e.message.content == 'islam')
   e.message.channel.sendMessage('"Religion of Peace" \n' + islam[islamI]);
 });
