31b95d53bf112b2afb3f099aa5768fc9be6d2389
MacBook <raze2736@gmail.com>
2017-12-14 04:07:30 -0500

    rpc

---
+++ rpc.js
@@ -0,0 +1,20 @@
+
+const { Client } = require('discord-rpc');
+
+const clientID = '390744285017800704';
+
+const client = new Client({ transport: 'ipc' });
+
+client.on('ready', () => {
+  console.log(Date.now() +' | '+ (Date.now()+817000))
+  client.setActivity({
+    state: 'Gassin\' Kikes',
+    details: '14 | 88',
+    startTimestamp: new Date(),
+    largeImageKey: 'sexysun',
+    smallImageKey: 'odal',
+    instance: true,
+  }).then(console.log);
+
+});
+client.login(clientID).catch(console.error);
