53bf554a3111dd2d3ee1a858c1c219c82223a999
MacBook <raze2736@gmail.com>
2018-01-20 22:31:35 -0500

    aa

--- dankmemes.js
+++ dankmemes.js
@@ -133,13 +133,14 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
       message.delete();
       channel.sendMessage('​( ͡° ͜ʖ( ͡° ͜ʖ ͡° )ʖ ͡° )╯╲___'+author.mention+' – Don\'t mind me just taking my **'+author.username+'** for a walk');
       if (author.roles.length == 0) {
-        try {
           author.setRoles(["268923144935440406", "401622676893859842"]);
           bella.once('1d', () => {
-            if (author) author.unassignRole("401622676893859842");
+            try {
+              if (author) author.unassignRole("401622676893859842");
+            } catch (e) {
+              console.log(e);
+            }
           });
-        } catch (e) {
-          console.log(e);
         }
       }
       else if (author.roles.length > 0){
